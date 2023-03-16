c3a004bb37a83c2653a95919c3a99eb66b0cb29c
Manoj <Manoj>
2023-03-16 16:51:53 +0530

    Java DataTypes solution

--- src/main/JavaDatatypes.java
+++ src/main/JavaDatatypes.java
@@ -18,12 +18,14 @@ public class JavaDatatypes {
 	                System.out.println(x+" can be fitted in:");
 	                if(x>=-128 && x<=127)System.out.println("* byte");
 	                //Complete the code
+	                if (x>= -32768 && x<= 32767) System.out.println("* short");
+	                if (x>= -2147483648 && x<= 2147483647 ) System.out.println("* int");
+	                if (x>= Long.MIN_VALUE && x<= Long.MAX_VALUE ) System.out.println("* long");
 	            }
 	            catch(Exception e)
 	            {
 	                System.out.println(sc.next()+" can't be fitted anywhere.");
 	            }
-
 	        }
 	        sc.close();
 	    }
