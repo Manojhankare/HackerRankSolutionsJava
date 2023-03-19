898310eb2a76ed736b9a8b522a153dfe57ddbd9b
Manoj <Manoj>
2023-03-19 15:30:26 +0530

    05 Java Loops I

---
+++ src/Solutions/JavaLoops1.java
@@ -0,0 +1,19 @@
+package Solutions;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class JavaLoops1 {
+	public static void main(String[] args) throws IOException {
+		BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+		int N = Integer.parseInt(bufferedReader.readLine().trim());
+
+		bufferedReader.close();
+		for (int i = 1; i <= 10; i++) {
+			int x = N * i;
+			System.out.println(N + " x " + i + " = " + x);
+		}
+	}
+}
